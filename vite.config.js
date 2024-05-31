57404fc602f0069945c02af356cffe4035a03e51
danny Darmawan <dannydarmawan35@gmail.com>
2024-05-31 13:29:43 +0700

    'ininambah'

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/profile-danny',
+  base: '/profile-danny/',
   plugins: [react()],
 })
