4abc1ec28f730e0dba5ae7d4ace95848b868d288
danny Darmawan <dannydarmawan35@gmail.com>
2024-05-31 12:17:28 +0700

    coba1

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -13,7 +13,7 @@ export default class App extends React.Component {
     return (
       <Router>
         <Routes>
-          <Route path="/" element={<RootHome />} />
+          <Route path="/profile-danny" element={<RootHome />} />
         </Routes>
       </Router>
     )
